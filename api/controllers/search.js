d8cce2aa2737f568cdb8287b333157166ed839da
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-05-17 04:22:32 +0700

    refactor!:  remove console.logs & upadte .env file

--- api/controllers/search.js
+++ api/controllers/search.js
@@ -8,14 +8,12 @@ module.exports = (req, res) => {
 	const count = req.query.count;
 	getTweets(query, count)
 		.then((response) => {
-			console.log(response.data);
 			res.status(200).send({
 				result: response.data,
 				status: "Successful Found all tweets",
 			});
 		})
 		.catch((error) => {
-			console.log(error);
 			res.status(400).send({
 				message: "Error not found tweets",
 				error: error,
--- api/helpers/search.js
+++ api/helpers/search.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 // API Endpoint to search tweets
 module.exports = (query, count) => {
 	const url = process.env.URL;
-	const token = process.env.BEARTOKEN;
+	const token = process.env.TWITTER_API_TOKEN;
 
 	return axios.get(url, {
 		params: {
